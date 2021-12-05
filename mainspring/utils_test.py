4b4d42e2cd9dd9f3192fd0456359cf992eed2082
jyoti.saha <jyoti.saha@telus.com>
2021-12-06 01:01:06 +0530

    initial setup

---
+++ mainspring/utils_test.py
@@ -0,0 +1,13 @@
+"""Unit tests for utils module."""
+
+import unittest
+
+from mainspring.corescheduler import utils
+
+
+class UtilsTest(unittest.TestCase):
+
+    def test_class_import_from_path(self):
+        path = 'mainspring.default_settings_test'
+        module = utils.import_from_path(path)
+        self.assertEqual(module.DEBUG, True)
---
+++ simple_scheduler/jobs/sample_job.py
@@ -0,0 +1,48 @@
+"""A sample job that prints string."""
+
+from mainspring.corescheduler import job
+
+
+class DemoJob(job.JobBase):
+
+    @classmethod
+    def meta_info(cls):
+        """
+
+        :return:
+        """
+        return {
+            'job_class_string': '%s.%s' % (cls.__module__, cls.__name__),
+            'notes': 'This will print a string in your shell. Check it out!',
+            'arguments': [
+                # argument1
+                {
+                    'type': 'string', 'description': 'First argument'
+                },
+
+                # argument2
+                {
+                    'type': 'string', 'description': 'Second argument'
+                }
+            ],
+            'example_arguments': '["first argument AAA", "second argument BBB"]'
+        }
+
+    @staticmethod
+    def run(argument1, argument2, *args, **kwargs):
+        """
+
+        :param argument1:
+        :param argument2:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        print('Hello from DemoJob! Argument1: %s, Argument2: %s' % (argument1, argument2))
+        return [argument1, argument2]
+
+
+if __name__ == "__main__":
+    # You can easily test this job here
+    job = DemoJob.create_test_instance()
+    job.run(123, 456)
